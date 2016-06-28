66c6df703da0cc7e9543b77b624760dd79e4599b
Cedric Rolland <c.rolland88@gmail.com>
2016-06-28 22:56:20 +0200

    added functions

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+const blague = [
+    "Ta mère est tellement grosse qu'il y a un décalage horaire entre ses deux fessses !",
+    "Deux secrétaires papotent pendant une pause. La première demande à l'autre :\n- Tu fumes après l'amour ?\n- Je ne sais pas, j'ai jamais regardé.",
+    "Ta vie. (outch, salo)",
+    "J'ai cru que Mozart était mort mais mozzarella (c'est une blague carambar)",
+    "C'est la blague de la cravate : elle est longue et plate. (mon dieu mais c'est tellement nul.)"
+]
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -51,11 +59,13 @@ app.post('/webhook/', function (req, res) {
                 }, 200);
             setTimeout(function() {
                   sendTextMessage(sender, "Tu peux me demander plein de trucs comme:\n- les coordonnées de la micronation\n- les dates du Scabryollaud");
-                }, 200);
+                }, 400);
         } else if (/.*[c,C]oordonn[e,é].+/.test(text)) {
             sendTextMessage(sender, "Les coordonnées de la micronation sont: 46.648059, 6.437642");
         } else if (/.*[s,S]cabr.+/.test(text) && /.*[d,D]ate.*/.test(text)) {
             sendTextMessage(sender, "le Scabryollaud se passe du 1er au 9 juillet du calendrier païen. (Be there, ça va être rigolo)");   
+        } else if (/.*[b,B]lague.*/.test(text)) {
+            sendTextMessage(sender, blague[getRandomInt(0, blague.length-1)]);   
         } else {
             sendTextMessage(sender, "Désolé on ne m'a pas encore codé pour répondre à ça.");   
             setTimeout(function() {
@@ -94,6 +104,10 @@ function sendTextMessage(sender, text) {
             console.log('Error: ', response.body.error)
         }
     })
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function sendGenericMessage(sender) {
